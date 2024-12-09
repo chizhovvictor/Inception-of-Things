6d2ef46e3fad8bdb2dd0c1040bab10ac08c44328
Victor Chizhov <victor.chizhov93@gmail.com>
2024-12-09 08:52:36 +0100

    WIP

--- p2/script/master.sh
+++ p2/script/master.sh
@@ -4,4 +4,4 @@ sudo apt-get install curl -y
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-iface eth1" sh -s - --token 12345
 
 
-#sudo kubectl apply -f /vagrant/script/app1.yaml
+sudo kubectl apply -f /vagrant/script/app1.yml
