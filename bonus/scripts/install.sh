947ddb0cd8ea7870e2102832646491ddf938967c
Victor Chizhov <victor.chizhov93@gmail.com>
2025-02-12 13:09:07 +0100

    WIP

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -14,11 +14,13 @@ sudo helm repo update
 sudo kubectl create namespace gitlab
 
 # install gitlab helm chart
-#sudo helm install gitlab gitlab/gitlab \
-#  --namespace gitlab \
-#  --set global.hosts.domain=gitlab-local.com\
-#  --set global.hosts.externalIP=10.1.1.1 \
-#  --set certmanager-issuer.email="huesosy@pidorasy.com"
+sudo helm install gitlab gitlab/gitlab \
+  --namespace gitlab \
+  -f https://gitlab.com/gitlab-org/charts/gitlab/raw/master/examples/values-minikube-minimum.yaml \
+  --set global.hosts.domain=gitlab-local.com \
+  --set global.hosts.externalIP=10.1.1.1 \
+  --set global.hosts.https=false
+  
 
 # Install helm chart
 helm install gitlab gitlab/gitlab -f value.yaml -n gitlab
@@ -38,52 +40,17 @@ helm list -n gitlab
 # Delete helm list if it exists
 helm uninstall gitlab -n gitlab
 
-#get passwd
+# Get passwd
 sudo kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode
 
-Шаг 6: Интеграция с Kubernetes
+# Get name of application
+sudo kubectl get applications --all-namespaces
 
-Для того чтобы GitLab мог работать с вашим кластером, необходимо настроить Kubernetes CI/CD:
+# Get repo url of application
+kubectl get application wil-playground -n argocd -o yaml | grep repoURL
 
-    Подключите ваш кластер Kubernetes к GitLab:
-        В интерфейсе GitLab перейдите в Admin Area → Kubernetes и добавьте ваш кластер.
-        Это позволит GitLab взаимодействовать с Kubernetes для развертывания приложений через CI/CD.
+# Create repo in gitlab and copy data from github in gitlab
 
-    Настройка GitLab Runner для работы с Kubernetes:
-
-        Установите GitLab Runner в Kubernetes для выполнения пайплайнов CI/CD.
-
-        Вы можете установить GitLab Runner с помощью Helm:
-
-        helm install gitlab-runner gitlab/gitlab-runner --namespace gitlab
-
-        Это создаст ресурс gitlab-runner в вашем кластере, который будет отвечать за запуск пайплайнов.
-
-Шаг 7: Настройка GitLab CI/CD
-
-    В вашем репозитории GitLab создайте файл .gitlab-ci.yml для определения пайплайнов.
-
-    Пример базового .gitlab-ci.yml:
-
-    stages:
-      - build
-      - deploy
-
-    build:
-      stage: build
-      script:
-        - echo "Building the application"
-
-    deploy:
-      stage: deploy
-      script:
-        - kubectl apply -f kubernetes/deployment.yaml
-
-    Создайте ресурсы Kubernetes (например, Deployment, Service, etc.) в каталоге kubernetes/ вашего проекта, чтобы GitLab мог деплоить их в ваш кластер.
-
-Шаг 8: Убедитесь, что все работает
-
-    Проверьте доступность GitLab по адресу http://localhost:8080.
-    Убедитесь, что GitLab может запускать пайплайны, деплоить приложения и работать с Kubernetes
-
+# Download repo from gitlab
+git clone 
 
